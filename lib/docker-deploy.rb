6da6b7cb96ab0b6a52a6ede1dde9b087ff0d2bed
Niko Dziemba <niko@dziemba.com>
2015-03-26 15:48:37 +0100

    Simplify rake task load

--- lib/docker-deploy.rb
+++ lib/docker-deploy.rb
@@ -1,11 +1,1 @@
-module DockerDeploy
-  if defined?(Rails)
-    class Railtie < Rails::Railtie
-      railtie_name :docker_deploy
-
-      rake_tasks do
-        load 'tasks/deploy.rake'
-      end
-    end
-  end
-end
+load 'tasks/deploy.rake'
